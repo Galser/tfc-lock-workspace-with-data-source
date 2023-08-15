d0f515b511c5d374b55208c5381f54982eea6c1e
Andrii Guselietov <guseletov@gmail.come>
2023-08-15 10:52:04 +0200

    Added varts for timeouts and gaps

--- main.tf
+++ main.tf
@@ -3,12 +3,24 @@ variable "todo_id" {
   default = 1
 }
 
+variable "api_timeout" {
+  type    = number
+  default = 1200
+}
+
+variable "safety_gap" {
+  type    = number
+  description = "Safety gap in milliseconds , we can make it negative to...fail on timeout"
+  default = -1
+}
+
 data "http" "this" {
-  url             = "https://jsonplaceholder.typicode.com/todos/${var.todo_id}"
+#  url             = "https://jsonplaceholder.typicode.com/todos/${var.todo_id}"
+  url             = "https://httpstat.us/200?sleep=${var.api_timeout}"
   request_headers = {
     Accept        = "application/json"
   }
- request_timeout_ms = 3000
+ request_timeout_ms = var.api_timeout*60+var.safety_gap
 }
 
 output "todo" {
